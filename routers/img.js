86bb07c18586f2b94c119b92ea52c2847a0ded69
HarshKumar Jha <jhaharsh878@gmail.com>
2022-08-31 21:28:44 +0530

    .

--- routers/img.js
+++ routers/img.js
@@ -3,30 +3,29 @@ const router = express.Router();
 const images = require('../modules/images');
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/gif', 'video/mp4'];
 
-router.post('/add', async(req, res)=>{
-   const {name, type, img} = req.body;
+router.post('/add', async (req, res) => {
+   const { name, type, img } = req.body;
    const imgs = new images({
       name,
-     type
+      type
    });
 
-   saveImage(imgs , img);
-   try{
+   saveImage(imgs, img);
+   try {
       const savedimg = await imgs.save();
       res.redirect('/');
-   }catch(error){
+   } catch (error) {
       console.error(error);
    }
 
 })
 
 // // save image as binary file
-function saveImage(imgs , imgEncoded){
-   if(imgEncoded == null) return;
+function saveImage(imgs, imgEncoded) {
+   if (imgEncoded == null) return;
    const imag = JSON.parse(imgEncoded);
-   console.log(imag);
-   if(imag!=null && imageMimeTypes.includes(imag.type)){
-      imgs.img =new Buffer.from(imag.data , 'base64');
+   if (imag != null && imageMimeTypes.includes(imag.type)) {
+      imgs.img = new Buffer.from(imag.data, 'base64');
       imgs.imgType = imag.type;
    }
 }
